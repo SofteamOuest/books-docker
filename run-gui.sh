c12a9872ae0b32c8cff6304877546ed23dc9b62a
Mehdi Elkouhen <mehdi.elkouhen@softeam.fr>
2017-12-06 22:34:13 +0100

    wip

--- run-gui.sh
+++ run-gui.sh
@@ -9,5 +9,6 @@ docker build . -t registry.k8.wildwidewest.xyz/repository/docker-repository/book
 docker run -d --name books-gui \
     --network=book-net \
     -p 80:80 \
+    -e books_api_url=books-api \
     -v $PWD/../books-gui:/usr/share/nginx/html \
     registry.k8.wildwidewest.xyz/repository/docker-repository/books/gui
